eb4884eab815548bc2d9cc143f00c9528e23d5eb
JuanGGil <jgg8979@nyu.edu>
2024-10-18 15:18:06 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -33,7 +33,8 @@ async def test_project(dut):
     dut.ui_in.value = (b>>8) + a
     dut.uio_in.value = 0 #Opcode ADD
     await ClockCycles(dut.clk, 10)
-    dut._log.info(f" 1. b: {b} a: {a} Opcode: ADD value of input is: {dut.ui_in.value} and value of output is {dut.ou_out.value}.")
+    dut._log.info(f" 1. b: {b} a: {a} Opcode: ADD")
+    dut._log.info(f" value of input is: {dut.ui_in.value} and value of output is {dut.ou_out.value}.")
     assert dut.uo_out.value == 2 and dut.uio_out.value == 0
 
     # Keep testing the module by changing the input values, waiting for
