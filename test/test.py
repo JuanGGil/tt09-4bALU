73a2d3b04967f87f3260f81bcc7428b32e4e0a62
JuanGGil <jgg8979@nyu.edu>
2024-10-24 12:40:10 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -49,7 +49,7 @@ async def test_project(dut):
             await ClockCycles(dut.clk, 10)
             dut._log.info(f" ADD case {i}. b: {b} a: {a} Opcode: ADD, value of input is: {dut.ui_in.value}, SUM: {dut.uo_out.value}, UIO: {dut.uio_out.value}")
             assert dut.uo_out.value == ((a+b) & 15) # SUM
-            assert (dut.uio_out.value >> 7) == (((a & 8) >> 2) & ((b & 8) >> 2) & ~(((a+b) & 8) >> 2)) or (~((a & 8) >> 2) & ~((b & 8) >> 2) & (((a+b) & 8) >> 2)) #Overflow
+            assert (dut.uio_out.value >> 7) == (((a & 8) >> 3) & ((b & 8) >> 3) & ~(((a+b) & 8) >> 3)) or (~((a & 8) >> 3) & ~((b & 8) >> 3) & (((a+b) & 8) >> 3)) #Overflow
             assert ((dut.uio_out.value & 64) >> 6) == (((a+b) & 16) >> 4) # Carryout
 
     # Keep testing the module by changing the input values, waiting for
